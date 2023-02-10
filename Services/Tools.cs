4ea81aa9d4edee7340a8355f316c561be98c8cde
Sacha <sachabasse@gmail.com>
2023-02-10 01:11:01 +0100

    Je deteste le jwt mais c'est pas pire que le JS

--- Services/Tools.cs
+++ Services/Tools.cs
@@ -1,6 +1,12 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.Intrinsics.Arm;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace ApiRessource2.Services
 {
@@ -9,7 +15,8 @@ namespace ApiRessource2.Services
 
         public static string HashCode(string password)
         {
-            string passwordHash = BCrypt.Net.BCrypt.HashPassword(password, "lol");
+            var token = new JwtSecurityToken();
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
             return passwordHash;
         }
     }
