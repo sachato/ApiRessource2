a92eb58107332b8f1327be2de23ec001772ccf8c
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-03-09 13:25:18 +0100

    Seed Ressource

--- ApiRessource2/DataContext.cs
+++ ApiRessource2/DataContext.cs
@@ -1,7 +1,6 @@
 ﻿using ApiRessource2.Models;
-using BCrypt.Net;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using OpenQA.Selenium;
 
 namespace ApiRessource2
 {
@@ -17,18 +16,20 @@ namespace ApiRessource2
         public DbSet<ReportedRessource> ReportedRessources { get; set; }
         public DbSet<Voted> Voteds { get; set; }
         public DbSet<ZoneGeo> ZoneGeos { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options, IWebHostEnvironment hostEnvironment)
             : base(options)
         {
-
             this.hostEnvironment = hostEnvironment;
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             if (hostEnvironment.IsDevelopment())
             {
                 modelBuilder.Entity<User>().HasData(new User { Id = 1, LastName = "Moreau", FirstName = "Kévin", IsConfirmed = true, IsDeleted = false, Role = Role.Administrator, CreationDate = DateTime.Now, Email = "kevin.moreau2@viacesi.fr", Password = BCrypt.Net.BCrypt.HashPassword("test"), PhoneNumber = "+33672920837", Username = "Keke", ZoneGeoId = 1 });
                 modelBuilder.Entity<User>().HasData(new User { Id = 2, LastName = "Tortelli", FirstName = "Sacha", IsConfirmed = true, IsDeleted = false, Role = Role.User, CreationDate = DateTime.Now, Email = "sacha.tortelli@viacesi.fr", Password = BCrypt.Net.BCrypt.HashPassword("test"), PhoneNumber = "+33672920835", Username = "Sachou", ZoneGeoId = 1 });
+                modelBuilder.Entity<Resource>().HasData(new Resource { Id = 1, CreationDate = DateTime.Now, Description = "Description1", DownVote = 0, UpVote = 0, IsDeleted = false, Path = "", Title = "Ressource1", Type = TypeRessource.Texte, UserId = 1 });
             }
         }
     }
